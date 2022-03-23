8e59dfd03736997721c8bbe643a3eec276282173
Cristian Krutki <cristian.krutki@gmail.com>
2022-03-23 19:10:42 -0300

    mock de task

---
+++ taskList/task-list/src/app/mock-task.ts
@@ -0,0 +1,23 @@
+import {Task} from "./task"
+export const TASK : Task[] = [
+{   id:1, 
+    text: "Terminar primero modulo de angular",
+    day: 'Agosto 5 a las 12:00',
+    reminder: true
+},
+{   id:2, 
+    text: "HAcer las compras",
+    day: 'Agosto 5 a las 17:00',
+    reminder: true
+},
+{   id:3, 
+    text: "Investigar sobre bootstrap",
+    day: 'Agosto 5 a las 19:00',
+    reminder: false
+},
+{   id:4, 
+    text: "Leer mi libros",
+    day: 'Agosto 5 a las 21:00',
+    reminder: false
+},
+]
