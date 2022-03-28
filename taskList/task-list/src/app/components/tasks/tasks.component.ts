d25391aa9bf96fccd65f8b9adc4f342d0568b37d
Cristian Krutki <cristian.krutki@gmail.com>
2022-03-28 17:04:53 -0300

    deleteTask Service

--- taskList/task-list/src/app/components/tasks/tasks.component.ts
+++ taskList/task-list/src/app/components/tasks/tasks.component.ts
@@ -18,5 +18,10 @@ export class TasksComponent implements OnInit {
       this.tasks = tasks;
     })
   }
+  deleteTask(task: Task){
+    this.tasksService.deleteTask(task).subscribe(()=>(
+      this.tasks=this.tasks.filter(t => t.id !== task.id)
+    ))
+  }
 
 }
